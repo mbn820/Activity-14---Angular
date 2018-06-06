a9bfbf749221ff73b65a4d57d22eafc7f8e932c8
mbn820 <mikhailbnunez@gmail.com>
2018-06-06 16:40:48 +0800

    Implemented Persons Page

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonsDataService } from '../persons-data.service';
 
 @Component({
   selector: 'app-persons',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persons.component.css']
 })
 export class PersonsComponent implements OnInit {
+  persons: any;
 
-  constructor() { }
+  constructor(private personsDataService: PersonsDataService) { }
 
   ngOnInit() {
+    this.loadAllPersons();
+  }
+
+  loadAllPersons() {
+    this.personsDataService
+        .getAllPersons()
+        .subscribe(persons => this.persons = persons);
+  }
+
+  deletePerson(personId) {
+    const decision = confirm('Are you sure?');
+    if(decision) {
+      this.personsDataService
+        .deletePerson(personId)
+        .subscribe(() => this.loadAllPersons());
+    }
   }
 
 }
